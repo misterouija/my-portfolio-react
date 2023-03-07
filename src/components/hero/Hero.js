4c354f317f2b6e18d2ff9765b83a27916665802c
Leighton Grant <oneal.grant@outlook.com>
2023-03-07 18:35:41 +0000

    Changes nav link colors

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -12,19 +12,19 @@ const Hero = () => {
     }, []);
 
     return (
-        <section id='hero' className='d-flex min-vh-100 align-items-center'>
+        <section id='hero' className='d-flex align-items-center'>
             <div
                 className='container d-flex flex-column align-items-center'
                 data-aos='zoom-in'
                 data-aos-delay='200'
             >
-                <h1 className='fw-bold my-2'>Leighton Grant</h1>
+                <h1 className='fw-bold my-2 fs-1'>Leighton Grant</h1>
                 <h2 className='fs-4 fw-light my-2'>
                     Professional Frontend Web Developer
                 </h2>
                 <a
                     href='about'
-                    className='btn btn-about btn-secondary rounded-pill px-5 my-4'
+                    className='btn btn-about rounded-pill px-5 my-4 HeroButton'
                 >
                     About Me
                 </a>
